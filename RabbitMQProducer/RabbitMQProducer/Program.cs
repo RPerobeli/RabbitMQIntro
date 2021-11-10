86b179da53c97fac80cfd6191726f4ab031572f3
Rodrigo <rodrigo.costa2015@engenharia.ufjf.br>
2021-11-10 10:11:43 -0300

    first commit

---
+++ RabbitMQProducer/RabbitMQProducer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQProducer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var factory = new ConnectionFactory
+                {
+                    Uri = new Uri("amqp://guest:guest@localhost:5672")
+                };
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
+                channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                var message = new { Name = "Producer", Message = "Hello!" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish("", "demo-queue", null, body);
+                Thread.Sleep(2000);
+            }
+            
+
+        }
+    }
+}
